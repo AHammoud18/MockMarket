5997a5fc8739113a358a39257ece4ae6afe94288
ahammoud <alihammoud120@gmail.com>
2023-02-03 10:09:49 -0500

    Initial Commit

---
+++ MockMarket/MockMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockMarketApp.swift
+//  MockMarket
+//
+//  Created by Ali Hamoud on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct MockMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
