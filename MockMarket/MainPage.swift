46674bae80f63a19c5f5efe9c9a69f9d3be8b536
Maverick Brazill <mjb172004@icloud.com>
2023-02-06 11:30:50 -0500

    UI for portfolio screen

--- MockMarket/MainPage.swift
+++ MockMarket/MainPage.swift
@@ -39,44 +39,247 @@ struct StockPage: View{
     let timer = Timer.publish(every: 10.00, on: .main, in: .common).autoconnect()
     let times = ["22-12-27", "23-01-27"]
     
+    @State var currentGraphTab: dateGraphTab = .today
+    enum dateGraphTab{
+        case today
+        case oneWk
+        case oneMo
+        case ytd
+        case oneYr
+    }
+    
+    func switchGraphTab() -> String{
+        switch self.currentGraphTab {
+        case .today:
+            return "Today"
+        case .oneWk:
+            return "OneWk"
+        case .oneMo:
+            return "OneMo"
+        case .ytd:
+            return "YTD"
+        case .oneYr:
+            return "oneYR"
+            //case allTime
+        }
+
+    }
+    
+    
     
     var body: some View{
         GeometryReader { geo in
             let X = geo.frame(in: .global).midX
             let Y = geo.frame(in: .global).midY
             ZStack{
-                Button{
-                    self.stockInfo.ticker = "AAPL"
-                    didSelectStock = true
-                }label:{
-                    Text("Apple Stock")
+//                Button{
+//                    self.stockInfo.ticker = "AAPL"
+//                    didSelectStock = true
+//                }label:{
+//                    Text("Apple Stock")
+//                }
+//                Button{
+//                    self.stockInfo.ticker = "IBM"
+//                    didSelectStock = true
+//                }label:{
+//                    Text("IBM Stock")
+//                }.offset(y: -20)
+//                Divider()
+                
+                
+                
+                
+                //MARK: Portfolio Top Graph
+                VStack(alignment:.leading) {
+                    Text("    My Portfolio")
+                        .font(.custom("American Typewriter", size: 24).bold())
+                    ZStack {
+                        GroupBox{
+                            LineChartView(
+                                lineChartController: LineChartController(
+                                    prices: self.userValue,
+                                    dates: self.userDates,
+                                    indicatorPointColor: self.pointPos.indicatorColor,
+                                    dragGesture: true
+                                )
+                            )
+                        }.frame(width: geo.frame(in: .global).maxX/1.2, height: geo.frame(in: .global).maxY/3)
+                            .position(x: X, y: Y/2.2)
+                        .groupBoxStyle(ChartBox())
+                        
+                        Text("Current: \(switchGraphTab())")
+                    }
+                    
+                    
+
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 30)
+                                .foregroundColor(.black)
+                                .frame(width: 360, height: 65)
+                                .position(x: (geo.frame(in: .local).midX + 10), y:(geo.frame(in: .local).midY/2.1))
+                            
+                            
+                            RoundedRectangle(cornerRadius: 30)
+                                .foregroundColor(.white)
+                                .frame(width: 360, height: 65)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 30)
+                                        .stroke(.black, lineWidth: 4))
+                                
+                                HStack {
+                                    ///today
+                                    Button {
+                                        currentGraphTab = dateGraphTab.today
+                                    }label: {
+                                        if currentGraphTab == dateGraphTab.today{
+                                            ZStack{
+                                                RoundedRectangle(cornerRadius: 10)
+                                                    .frame(width: 70, height: 40)
+                                                    .foregroundColor(.black)
+                                                Text("Today")
+                                                    .foregroundColor(.white)
+                                                    .font(.custom("American Typewriter", size: 16).bold())
+                                                
+                                            }
+                                        } else{
+                                            ZStack{
+                                                RoundedRectangle(cornerRadius: 10)
+                                                    .frame(width: 70, height: 40)
+                                                    .foregroundColor(.white)
+                                                Text("Today")
+                                                    .foregroundColor(.black)
+                                                    .font(.custom("American Typewriter", size: 16).bold())
+                                                
+                                            }
+                                        }
+                                    }
+                                    ///one week
+                                    Button {
+                                        currentGraphTab = dateGraphTab.oneWk
+                                    }label: {
+                                        if currentGraphTab == dateGraphTab.oneWk{
+                                            ZStack{
+                                                RoundedRectangle(cornerRadius: 10)
+                                                    .frame(width: 50, height: 40)
+                                                    .foregroundColor(.black)
+                                                Text("1W")
+                                                    .foregroundColor(.white)
+                                                    .font(.custom("American Typewriter", size: 16).bold())
+                                                
+                                            }
+                                        } else{
+                                            ZStack{
+                                                RoundedRectangle(cornerRadius: 10)
+                                                    .frame(width: 50, height: 40)
+                                                    .foregroundColor(.white)
+                                                Text("1W")
+                                                    .foregroundColor(.black)
+                                                    .font(.custom("American Typewriter", size: 16).bold())
+                                                
+                                            }
+                                        }
+                                    }
+                                    /// one month
+                                    Button {
+                                        currentGraphTab = dateGraphTab.oneMo
+                                    }label: {
+                                        if currentGraphTab == dateGraphTab.oneMo{
+                                            ZStack{
+                                                RoundedRectangle(cornerRadius: 10)
+                                                    .frame(width: 50, height: 40)
+                                                    .foregroundColor(.black)
+                                                Text("1M")
+                                                    .foregroundColor(.white)
+                                                    .font(.custom("American Typewriter", size: 16).bold())
+                                                
+                                            }
+                                        } else{
+                                            ZStack{
+                                                RoundedRectangle(cornerRadius: 10)
+                                                    .frame(width: 50, height: 40)
+                                                    .foregroundColor(.white)
+                                                Text("1M")
+                                                    .foregroundColor(.black)
+                                                    .font(.custom("American Typewriter", size: 16).bold())
+                                                
+                                            }
+                                        }
+                                    }
+                                    ///ytd
+                                    Button {
+                                        currentGraphTab = dateGraphTab.ytd
+                                    }label: {
+                                        if currentGraphTab == dateGraphTab.ytd{
+                                            ZStack{
+                                                RoundedRectangle(cornerRadius: 10)
+                                                    .frame(width: 60, height: 40)
+                                                    .foregroundColor(.black)
+                                                Text("YTD")
+                                                    .foregroundColor(.white)
+                                                    .font(.custom("American Typewriter", size: 16).bold())
+                                                
+                                            }
+                                        } else{
+                                            ZStack{
+                                                RoundedRectangle(cornerRadius: 10)
+                                                    .frame(width: 60, height: 40)
+                                                    .foregroundColor(.white)
+                                                Text("YTD")
+                                                    .foregroundColor(.black)
+                                                    .font(.custom("American Typewriter", size: 16).bold())
+                                                
+                                            }
+                                        }
+                                    }
+                                    /// one year
+                                    Button {
+                                        currentGraphTab = dateGraphTab.oneYr
+                                    }label: {
+                                        if currentGraphTab == dateGraphTab.oneYr{
+                                            ZStack{
+                                                RoundedRectangle(cornerRadius: 10)
+                                                    .frame(width: 40, height: 40)
+                                                    .foregroundColor(.black)
+                                                Text("1Y")
+                                                    .foregroundColor(.white)
+                                                    .font(.custom("American Typewriter", size: 16).bold())
+                                                
+                                            }
+                                        } else{
+                                            ZStack{
+                                                RoundedRectangle(cornerRadius: 10)
+                                                    .frame(width: 40, height: 40)
+                                                    .foregroundColor(.white)
+                                                Text("1Y")
+                                                    .foregroundColor(.black)
+                                                    .font(.custom("American Typewriter", size: 16).bold())
+                                                
+                                            }
+                                        }
+                                    }
+                                    
+                                    
+                                    
+                                    
+                                }
+                                .frame(width:350, height: 50)
+                                .clipShape(RoundedRectangle(cornerRadius: 30))
+                                //.frame(height: 50)
+                          
+                            
+                        //.padding(.bottom)
+                        }
+                        .position(x: geo.frame(in: .local).midX, y: geo.frame(in: .local).midY/9)
+                        .padding((.bottom))
+                    }
                 }
-                Button{
-                    self.stockInfo.ticker = "IBM"
-                    didSelectStock = true
-                }label:{
-                    Text("IBM Stock")
-                }.offset(y: -20)
-                Divider()
-                GroupBox{
-                    LineChartView(
-                        lineChartController: LineChartController(
-                            prices: self.userValue,
-                            dates: self.userDates,
-                            indicatorPointColor: self.pointPos.indicatorColor,
-                            dragGesture: true
-                        )
-                    )
-                }.frame(width: geo.frame(in: .global).maxX/1.2, height: geo.frame(in: .global).maxY/3)
-                    .position(x: X, y: Y*1.4)
-                    .groupBoxStyle(ChartBox())
             }
             .sheet(isPresented: $didSelectStock, onDismiss: { self.stockInfo.deintitStock() }){
                 LoadingScreen()
             }
         }
     }
-}
+
 
 
 struct LoadingScreen: View{
@@ -216,30 +419,35 @@ struct ChartBox: GroupBoxStyle{
     func makeBody(configuration: Configuration) -> some View {
         
         GeometryReader{ geo in
-            RoundedRectangle(cornerRadius: 12)
+            RoundedRectangle(cornerRadius: 30)
                 .foregroundColor(.white)
-                .shadow(radius: 4, x: 5)
-                .shadow(radius: 4, x: 5)
+                //.shadow(radius: 4, x: 5)
+                //.shadow(radius: 4, x: 5)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 30)
+                        .stroke(Color.black, lineWidth: 6)
+                    )
                 .overlay(
                     Rectangle()
-                        .frame(width: 1, height: geo.size.height)
+                        .frame(width: 1, height: geo.size.height + 90)
                         .opacity(self.pointPos.showingIndicator ? 1.0 : 0.0)
-                        .position(x: self.pointPos.indicatorPosition.x, y: geo.size.height * 0.5)
+                        .position(x: self.pointPos.indicatorPosition.x, y: geo.size.height)
                         .foregroundColor(pointPos.indicatorColor)
                         .shadow(color: pointPos.indicatorColor, radius: 2)
                         .clipped()
                 )
-                
-                .overlay(
-                    Text("9:30")    
-                        .position(x: .zero, y: geo.size.height * 1.05)
-                        .font(Font.system(size: 8))
-                )
+//                .overlay(
+//                    Text("9:30")
+//                        .position(x: .zero, y: geo.size.height * 1.05)
+//                        .font(Font.system(size: 8))
+//                )
                 .overlay(
                     Text("\(self.stockInfo.currentTime)")
                         .position(x: geo.size.width, y: geo.size.height*1.05)
                         .font(Font.system(size: 8))
                 )
+                .padding(EdgeInsets(top:-50, leading: -3, bottom: -5, trailing: -3))
+
                 
             configuration.label
             configuration.content
@@ -248,7 +456,11 @@ struct ChartBox: GroupBoxStyle{
     
 }
 
-
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        StockPage()
+    }
+}
 
 
 /*
