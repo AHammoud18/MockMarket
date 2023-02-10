5af047e2358856a1f47c2a96cd1b667121c92d62
ahammoud <alihammoud120@gmail.com>
2023-02-10 11:42:47 -0500

    Updated portfolio data file

--- MockMarket/PortfolioData.swift
+++ MockMarket/PortfolioData.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import SwiftyJSON
+
+
+
+class Portfolio: ObservableObject{
+    @Published var userData: [String : [Int : Double]] = [:]
+    let appPath = Bundle.main.resourceURL
+    
+    func loadFile(){
+
+        //logic to read JSON file
+        
+    }
+    
+    @MainActor func writeFile(){
+        
+        userData["AAPL"] = [12 : 141.22]
+
+        let file = JSON(userData)
+        print(file)
+    }
+    
+  
+    
+    
+    
+}
